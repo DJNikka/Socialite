7e4756108bb867f9574eb184c671835ee4709241
Nikka-Sher <npiterman@gmail.com>
2017-07-20 23:14:15 -0700

    Initial Commit

---
+++ Socialite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Socialite
+//
+//  Created by Konstantine Piterman on 7/20/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
